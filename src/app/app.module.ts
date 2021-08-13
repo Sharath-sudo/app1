cb34b0e706f1783bd23b602c3655ff5a2ddda219
sharath kumar <sharath.dev03@gmail.com>
2021-08-12 23:29:13 -0400

    Overrided default angular properties and added first server component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ServerComponent } from './server/server.component';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
     AppComponent
+    ServerComponent
   ],
   imports: [
     BrowserModule
