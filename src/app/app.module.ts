02d7c6d8d7ea3ee611c2860fd899d61e84102f0e
sharath kumar <sharath.dev03@gmail.com>
2021-08-13 19:34:56 -0400

    Created two own basic components and styled them in two different ways

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ServerComponent } from './server/server.component';
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './Warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+
+
+
+// .ts extension is automatically added by the webpack
+
+// Typescript feature
+
 
 @NgModule({
   declarations: [
-    AppComponent
-    ServerComponent
+    AppComponent,
+    WarningAlertComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
